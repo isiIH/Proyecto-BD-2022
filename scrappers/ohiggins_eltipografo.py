b75928855e2b77b2164e99c72fa43c16f317a4e7
Isi <isidora.hernandez@alumnos.uach.cl>
2022-06-25 22:31:55 -0400

    puse algunos scrappers

---
+++ scrappers/ohiggins_eltipografo.py
@@ -0,0 +1,74 @@
+import random
+from requests_html import HTMLSession
+import w3lib.html
+import html
+
+def format_date(date):
+        return(date.split("T")[0])
+
+session = HTMLSession()
+
+URL = "https://eltipografo.cl/inicio"
+
+USER_AGENT_LIST = [
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/22.0.1207.1 Safari/537.1",
+        "Mozilla/5.0 (X11; CrOS i686 2268.111.0) AppleWebKit/536.11 (KHTML, like Gecko) Chrome/20.0.1132.57 Safari/536.11",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.6 (KHTML, like Gecko) Chrome/20.0.1092.0 Safari/536.6",
+        "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.6 (KHTML, like Gecko) Chrome/20.0.1090.0 Safari/536.6",
+        "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/19.77.34.5 Safari/537.1",
+        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.9 Safari/536.5",
+        "Mozilla/5.0 (Windows NT 6.0) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.36 Safari/536.5",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3",
+        "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_0) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3",
+        "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1062.0 Safari/536.3",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1062.0 Safari/536.3",
+        "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",
+        "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",
+        "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.0 Safari/536.3",
+        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/535.24 (KHTML, like Gecko) Chrome/19.0.1055.1 Safari/535.24",
+        "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/535.24 (KHTML, like Gecko) Chrome/19.0.1055.1 Safari/535.24"
+]
+headers = {'user-agent':random.choice(USER_AGENT_LIST) }
+
+response = session.get(URL,headers=headers)
+
+xpath_url="//app-home//div/a/@href"
+
+all_urls = response.html.xpath(xpath_url)[0:5]
+
+i = 1
+for url in all_urls:
+        print(f"----------\nNoticia {i}:\n----------\n")
+
+        #URL
+        article_url = "https://eltipografo.cl" + url
+        print(article_url)
+        response = session.get(article_url,headers=headers)
+
+        #FECHA DE PUBLICACION
+        xpath_date="//div[@class='container']//small[@class='grey py-1 my-2']"
+        date = response.html.xpath(xpath_date)[0].text
+        print(date)
+
+        #TITULO
+        xpath_title="//div//h1"
+        title = response.html.xpath(xpath_title)[0].text
+        print(title)
+
+        #CONTENIDO
+        xpath_texto="//div[@class='col-lg-8 col-sm-12 col-md-9']//p"
+        list_p = response.html.xpath(xpath_texto)
+
+        texto=""
+        for p in list_p:
+                content = p.text
+                content = w3lib.html.remove_tags(content)
+                content = w3lib.html.replace_escape_chars(content)
+                content = html.unescape(content)
+                content = content.strip()
+                texto=texto+" "+content
+
+        print(texto, "\n")
+        i += 1
